7f1b0f1ac8be99068e9f365f23a43e87c2986ec8
Chris Burroughs <chris.burroughs@joyent.com>
2017-08-16 14:50:18 -0400

    integration tests in multiple modules demo

---
+++ java-manta-client/src/test/java/com/joyent/manta/DemoIT.java
@@ -0,0 +1,13 @@
+package com.joyent.manta;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class DemoIT {
+
+    @Test
+    public void canDoADemo() {
+        Assert.assertTrue(false);
+    }
+}
